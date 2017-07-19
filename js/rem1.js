d63db639a58b7f683876fc35422ac04e0540228d
huanglsj <252585541@qq.com>
2017-07-19 10:20:54 +0800

    更新

--- js/rem1.js
+++ js/rem1.js
@@ -4,13 +4,12 @@
            recalc = function () {
                var clientWidth = docEl.clientWidth;
                if (!clientWidth) return;
-               if(clientWidth>=640){
-                   docEl.style.fontSize = '100px';
+               if(clientWidth>=750){
+                   docEl.style.fontSize = '50px';
                }else{
-                   docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
+                   docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
                }
            };
- 
        if (!doc.addEventListener) return;
        win.addEventListener(resizeEvt, recalc, false);
        doc.addEventListener('DOMContentLoaded', recalc, false);
